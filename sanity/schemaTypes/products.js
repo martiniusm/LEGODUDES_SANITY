16c38f5c78537b36e9ba04c470a5dc88e5c9d389
martiniusm <martimob@hiof.no>
2024-03-21 09:40:40 +0100

    hh

---
+++ sanity/schemaTypes/products.js
@@ -0,0 +1,31 @@
+export const products = {
+    title: "Produkter",
+    name: "products",
+    type: "document",
+    fields: [
+        {
+            title: "Produktnavn",
+            name: "productname",
+            type: "string"
+        },
+        {
+            title: "Produktlink",
+            name: "producturl",
+            type: "slug",
+            options: {
+                source: "productname",
+                slugify: input => input.toLowerCase().replace(/\s+/g, "-")
+            }
+        },
+        {
+            title: "Pris",
+            name: "price",
+            type: "number"
+        },
+        {
+            title: "Produktbilde",
+            name: "productimage",
+            type: "image"
+        }
+    ]
+}
